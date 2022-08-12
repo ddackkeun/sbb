00d30985c62eada77d0783ca299389ef336d83f1
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-08-12 14:14:26 +0900

    test : 컨트롤러 실행 확인 Controller 생성
    GetMapping, PostMapping 확인

---
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -0,0 +1,43 @@
+package com.ll.exam.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MainController {
+
+    @ResponseBody
+    @GetMapping("/sbb")
+    public String showMain() {
+        return "안녕하세요";
+    }
+
+    @GetMapping("/page1")
+    @ResponseBody
+    public String showPage1() {
+        return """
+                <form method="POST" action="/page2">
+                    <input type="number" name="age" placeholder="나이" />
+                    <input type="submit" value="page2로 POST 방식으로 이동" />
+                </form>
+                """;
+    }
+
+    @PostMapping("/page2")
+    @ResponseBody
+    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+
+    @GetMapping("/page2")
+    @ResponseBody
+    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
+                """.formatted(age);
+    }
+}
