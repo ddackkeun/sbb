f429ffc31f2e542d38e5aae820443069ab9ea416
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-08-12 14:55:41 +0900

    [추가] PathVariable, RequestParam 사용

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,6 +3,9 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 @Controller
 public class MainController {
 
@@ -40,4 +43,47 @@ public class MainController {
                 <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
                 """.formatted(age);
     }
+
+    @ResponseBody
+    @GetMapping("/plus")
+    public int showPlus(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String showGugudan(Integer dan, Integer limit) {
+
+        if (limit == null) {
+            limit = 9;
+        }
+
+        if (dan == null) {
+            dan = 9;
+        }
+
+        Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan *i))
+                .collect(Collectors.joining("<br>"));
+    }
+
+    @ResponseBody
+    @GetMapping("/mbti/{name}")
+    public String showMbti(@PathVariable String name) {
+        String result = switch (name) {
+            case "홍길동" -> "INFP";
+            case "임꺽정" -> "ENFP";
+            case "김철수" -> "ISTJ";
+            default -> "모름";
+        };
+        return result;
+    }
+
 }
