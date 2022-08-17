a859c6dea0f0243d60451abe3ea90133e4531bbd
seungkeun <tmdrms0705@gmail.com>
2022-08-18 04:00:51 +0900

    FEAT : truncate 사용을 위한 설정 변경
    - QuestionRepository truncate() 메소드 추가
    - Answer @JoinColumn의 forign key 설정 변경
    - testJpa0()을 통한 실행확인

--- src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/exam/sbb/SbbApplicationTests.java
@@ -19,7 +19,25 @@ class SbbApplicationTests {
 	private QuestionRepository questionRepository;
 
 	@Test
-	void testJpa() {
+	void testJpa0() {
+		// 테스트 롤백을 하지 않았기 때문에 DB에 계속해서 저장되어 있는다.
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q1);  // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		questionRepository.save(q2);  // 두번째 질문 저장
+
+		questionRepository.truncate();
+	}
+
+	@Test
+	void testJpa1() {
 		// 테스트 롤백을 하지 않았기 때문에 DB에 계속해서 저장되어 있는다.
 		Question q1 = new Question();
 		q1.setSubject("sbb가 무엇인가요?");
