a6e233fd18c4e3ad671d7f0398fa2f5c4520090b
SeungKeun Choi <77659341+ddackkeun@users.noreply.github.com>
2022-08-19 23:58:04 +0900

    FEAT : Question 관련 요청 추가
    - 기본 URL 요청시QuestionContllor에 의해 question list 페이지로 이동하는  redirect 추가
    - Repository와 연결하는 QuestionService 추가
    - question_list.html 내부 list 출력하는 로직 추가

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -21,166 +21,100 @@ public class MainController {
         return "안녕하세요";
     }
 
-    @GetMapping("/page1")
-    @ResponseBody
-    public String showPage1() {
-        return """
-                <form method="POST" action="/page2">
-                    <input type="number" name="age" placeholder="나이" />
-                    <input type="submit" value="page2로 POST 방식으로 이동" />
-                </form>
-                """;
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
-
-    @PostMapping("/page2")
-    @ResponseBody
-    public String showPage2Post(@RequestParam(defaultValue = "0") int age) {
-        return """
-                <h1>입력된 나이 : %d</h1>
-                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
-                """.formatted(age);
-    }
-
-    @GetMapping("/page2")
-    @ResponseBody
-    public String showPage2Get(@RequestParam(defaultValue = "0") int age) {
-        return """
-                <h1>입력된 나이 : %d</h1>
-                <h1>안녕하세요, POST 방식으로 오셨군요.</h1>
-                """.formatted(age);
-    }
-
-    @ResponseBody
-    @GetMapping("/plus")
-    public int showPlus(int a, int b) {
-        return a + b;
-    }
-
-    @GetMapping("/minus")
-    @ResponseBody
-    public int showMinus(int a, int b) {
-        return a - b;
-    }
-
-    @GetMapping("/gugudan")
-    @ResponseBody
-    public String showGugudan(Integer dan, Integer limit) {
-
-        if (limit == null) {
-            limit = 9;
-        }
-
-        if (dan == null) {
-            dan = 9;
-        }
-
-        Integer finalDan = dan;
-        return IntStream.rangeClosed(1, limit)
-                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
-                .collect(Collectors.joining("<br>"));
-    }
-
-    @ResponseBody
-    @GetMapping("/mbti/{name}")
-    public String showMbti(@PathVariable String name) {
-        String result = switch (name) {
-            case "홍길동" -> "INFP";
-            case "임꺽정" -> "ENFP";
-            case "김철수" -> "ISTJ";
-            default -> "모름";
-        };
-        return result;
-    }
-
-    @ResponseBody
-    @GetMapping("/saveSession/{name}/{value}")
-    public String saveSession(@PathVariable String name,
-                              @PathVariable String value,
-                              HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        session.setAttribute(name, value);
-
-        return "세션 변수 %s의 값이 %s로 설정되었습니다.".formatted(name, value);
-    }
-
-    @ResponseBody
-    @GetMapping("/getSession/{name}")
-    public String getSession(@PathVariable String name, HttpSession session) {
-        String value = (String) session.getAttribute(name);
-        return "세션변수 %s의 값이 %s 입니다.".formatted(name, value);
-    }
-
-
-    private List<Article> articles = new ArrayList<>(
-            Arrays.asList(new Article("제목", "내용"), new Article("제목", "내용"))
-    );
-    // asList의 결과 값은 immutable(수정할 수 없는) 리스트이다.
-    // ArrayList 로 감싸면 mutable로 바뀐다.
-
-    @ResponseBody
-    @GetMapping("/addArticle")
-    public String addArticle(String title, String body) {
-        Article article = new Article(title, body);
-        articles.add(article);
-
-        return "%d번 게시물이 생성되었습니다.".formatted(article.getId());
-    }
-
-    @ResponseBody
-    @GetMapping("/article/{id}")
-    public Article getArticle(@PathVariable int id) {
-        Article article = articles
-                .stream()
-                .filter(a -> a.getId() == id)
-                .findFirst()
-                .orElse(null);
-
-        return article;
-    }
-
-    @ResponseBody
-    @GetMapping("/modifyArticle/{id}")
-    public String modifyArticle(@PathVariable int id, String title, String body) {
-        Article article = articles
-                .stream()
-                .filter(a -> a.getId() == id)
-                .findFirst()
-                .orElse(null);
-
-        if (article == null) {
-            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
-        }
-
-        article.setTitle(title);
-        article.setBody(body);
-
-        return "%d번 게시물을 수정하였습니다.".formatted(id);
-    }
-
-    @ResponseBody
-    @GetMapping("/deleteArticle/{id}")
-    public String deleteArticle(@PathVariable int id) {
-        Article article = articles
-                .stream()
-                .filter(a -> a.getId() == id)
-                .findFirst()
-                .orElse(null);
-
-        if (article == null) {
-            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
-        }
-
-        articles.remove(article);
-
-        return "%d번 게시물을 삭제하였습니다.".formatted(article.getId());
-    }
-
-    @ResponseBody
-    @GetMapping("/addPerson/{id}")
-    public Person addPerson(Person person) {
-        // Param 값으로 들어온 것을
-        // @ModelAttribute 로 객체를 자동 매핑해준다.
-        // PathVariable의 값도 쓰지 않아도 자동으로 매핑해서 넣어준다.
-        return person;
-    }
+//
+//    @ResponseBody
+//    @GetMapping("/saveSession/{name}/{value}")
+//    public String saveSession(@PathVariable String name,
+//                              @PathVariable String value,
+//                              HttpServletRequest request) {
+//        HttpSession session = request.getSession();
+//        session.setAttribute(name, value);
+//
+//        return "세션 변수 %s의 값이 %s로 설정되었습니다.".formatted(name, value);
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/getSession/{name}")
+//    public String getSession(@PathVariable String name, HttpSession session) {
+//        String value = (String) session.getAttribute(name);
+//        return "세션변수 %s의 값이 %s 입니다.".formatted(name, value);
+//    }
+//
+//
+//    private List<Article> articles = new ArrayList<>(
+//            Arrays.asList(new Article("제목", "내용"), new Article("제목", "내용"))
+//    );
+//    // asList의 결과 값은 immutable(수정할 수 없는) 리스트이다.
+//    // ArrayList 로 감싸면 mutable로 바뀐다.
+//
+//    @ResponseBody
+//    @GetMapping("/addArticle")
+//    public String addArticle(String title, String body) {
+//        Article article = new Article(title, body);
+//        articles.add(article);
+//
+//        return "%d번 게시물이 생성되었습니다.".formatted(article.getId());
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/article/{id}")
+//    public Article getArticle(@PathVariable int id) {
+//        Article article = articles
+//                .stream()
+//                .filter(a -> a.getId() == id)
+//                .findFirst()
+//                .orElse(null);
+//
+//        return article;
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/modifyArticle/{id}")
+//    public String modifyArticle(@PathVariable int id, String title, String body) {
+//        Article article = articles
+//                .stream()
+//                .filter(a -> a.getId() == id)
+//                .findFirst()
+//                .orElse(null);
+//
+//        if (article == null) {
+//            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+//        }
+//
+//        article.setTitle(title);
+//        article.setBody(body);
+//
+//        return "%d번 게시물을 수정하였습니다.".formatted(id);
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/deleteArticle/{id}")
+//    public String deleteArticle(@PathVariable int id) {
+//        Article article = articles
+//                .stream()
+//                .filter(a -> a.getId() == id)
+//                .findFirst()
+//                .orElse(null);
+//
+//        if (article == null) {
+//            return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+//        }
+//
+//        articles.remove(article);
+//
+//        return "%d번 게시물을 삭제하였습니다.".formatted(article.getId());
+//    }
+//
+//    @ResponseBody
+//    @GetMapping("/addPerson/{id}")
+//    public Person addPerson(Person person) {
+//        // Param 값으로 들어온 것을
+//        // @ModelAttribute 로 객체를 자동 매핑해준다.
+//        // PathVariable의 값도 쓰지 않아도 자동으로 매핑해서 넣어준다.
+//        return person;
+//    }
 }
